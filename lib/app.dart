7bd188c0984ac4226396bc4089ff8a1a2021ea97
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2026-05-22 19:37:52 -0500

    Feat: Renaming some files to Flutter MVVM format

--- lib/app.dart
+++ lib/app.dart
@@ -5,12 +5,12 @@ import 'ui/home/screens/feature_home_screen.dart';
 import 'ui/home/screens/feature_home_screen.dart';
 import 'ui/home/screens/feature_home_screen.dart';
 import 'ui/home/screens/feature_home_screen.dart';
-import 'ui/feature_users_management/widgets/login_light_screen.dart';
-import 'ui/feature_users_management/widgets/login_dark_screen.dart';
-import 'ui/feature_users_management/widgets/form_light_screen.dart';
-import 'ui/feature_users_management/widgets/form_dark_screen.dart';
-import 'ui/feature_users_management/widgets/learning_module_light_screen.dart';
-import 'ui/feature_users_management/widgets/learning_module_dark_screen.dart';
+import 'ui/users_management/screens/login_light_screen.dart';
+import 'ui/users_management/screens/login_dark_screen.dart';
+import 'ui/users_management/screens/form_light_screen.dart';
+import 'ui/users_management/screens/form_dark_screen.dart';
+import 'ui/users_management/screens/learning_module_light_screen.dart';
+import 'ui/users_management/screens/learning_module_dark_screen.dart';
 
 // Definimos los 6 estados de tema posibles de tu TG
 enum AppThemeMode {
